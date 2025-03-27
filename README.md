544f32bee787ff495d3b098c22bb8b5d8339d3ff
younminchan <dbsalscks0@naver.com>
2025-03-27 14:20:29 +0900

    Create Project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Jetpack_Compose
-Android Jetpack_Compose Example source
+Android Jetpack_Compose Example source  
+
+
+## Jetpack Compose 기초  
+https://developer.android.com/codelabs/jetpack-compose-basics?hl=ko&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%3Fhl%3Dko%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-basics#0
+
+
+## Jetpack Compose 애니메이션  
+https://developer.android.com/codelabs/jetpack-compose-animation?hl=ko#0
---
+++ app/src/main/java/com/compose/basic/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.compose.basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.basic.ui.theme.Jetpack_compose_basicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_compose_basicTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Jetpack_compose_basicTheme {
+        Greeting("Android")
+    }
+}
